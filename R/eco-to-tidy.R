ac91ad083b5653f90b67f98076b9d43f76a6a9aa
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-17 13:53:15 +0100

    Based on benchmark 99,9% is due to var.get.nc call.

--- R/eco-to-tidy.R
+++ R/eco-to-tidy.R
@@ -1,5 +1,8 @@
 #' @examples
 #' nc <- system.file(package = "ecocat", "extdata/volume.nc")
+#' volume <- eco_to_tidy(nc)
+#'
+#' volume <- eco_to_tidy("z:/my_data_alex/Markus/ECOHAM_B057/B057-D4_3D.2000.vol.nc")
 
 eco_to_tidy <- function(nc) {
   nc_read <- RNetCDF::open.nc(con = nc)
@@ -18,5 +21,21 @@ eco_to_tidy <- function(nc) {
   # 2. Latitude
   # 3. Depth
   # 4. Time
+  d <- dim(ar)
 
+  # create columns (vectorised) with netcdf variable values.
+  vars_new <- purrr::map(variables[-length(variables)], RNetCDF::var.get.nc, ncfile = nc_read)
+  c1 <- rep(rep(rep(vars_new[[1]], times = d[2]), times = d[3]), times = d[4])
+  c2 <- rep(rep(rep(vars_new[[2]], each = d[1]), times = d[3]), times = d[4])
+  c3 <- rep(rep(vars_new[[3]], each = d[1] * d[2]), times = d[4])
+  c4 <- rep(vars_new[[4]], each = d[1] * d[2] * d[3])
+
+  # Combine to tibble and remove missing values.
+  ecodf <- tibble::tibble(c1, c2, c3, c4, c5 = as.vector(ar)) %>%
+    dplyr::filter_(~!is.na(c5)) %>%
+    purrr::set_names(variables)
+
+  return(ecodf)
 }
+
+
