7aff881720863908403e820ace981cdd5dd5ba84
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-17 16:59:11 +0100

    update tests.

---
+++ tests/testthat/test-eco-to-tidy.R
@@ -0,0 +1,7 @@
+context("test specific values in dataframe")
+
+vol <- eco_to_tidy(system.file(package = "ecocat", "extdata/volume.nc"))
+
+test_that("test this", {
+  expect_equal(dim(vol), c(17638, 5))
+})
--- tests/testthat/test-hello.R
+++
@@ -1,5 +0,0 @@
-context("initial test")
-
-test_that("test this", {
-  expect_equal(1 + 1, 2)
-})
