a018e08343e7b043a1946298f640cd979f607025
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-17 17:28:15 +0100

    Add ecoham box id.

--- R/eco-to-tidy.R
+++ R/eco-to-tidy.R
@@ -4,16 +4,16 @@
 #' coupling with atlantistools.
 #'
 #' @param nc connection to the netcdf file to read in.
-#' @return tidy dataframe with columns longitude, latitude, depth, time and variable-name.
+#' @return tidy dataframe with columns longitude, latitude, depth, time, variable-name, ecohambox-id.
 #' @export
 #'
 #' @examples
 #' nc <- system.file(package = "ecocat", "extdata/volume.nc")
-#' volume <- eco_to_tidy(nc)
+#' vol <- eco_to_tidy(nc)
 # nc <- "z:/my_data_alex/Markus/ECOHAM_B057/B057-D4_3D.2000.vol.nc"
 # volume <- eco_to_tidy("z:/my_data_alex/Markus/ECOHAM_B057/B057-D4_3D.2000.vol.nc")
 
-eco_to_tidy <- function(nc) {
+eco_to_tidy <- function(nc, nicemap) {
   nc_read <- RNetCDF::open.nc(con = nc)
   on.exit(RNetCDF::close.nc(nc_read))
 
@@ -38,13 +38,18 @@ eco_to_tidy <- function(nc) {
   c2 <- rep(rep(rep(vars_new[[2]], each = d[1]), times = d[3]), times = d[4])
   c3 <- rep(rep(vars_new[[3]], each = d[1] * d[2]), times = d[4])
   c4 <- rep(vars_new[[4]], each = d[1] * d[2] * d[3])
+  c6 <- rep(1:(d1[1] * d[2]), times = d[3] * d[4])
 
   # Combine to tibble and remove missing values.
-  ecodf <- tibble::tibble(c1, c2, c3, c4, c5 = as.vector(ar)) %>%
-    dplyr::filter_(~!is.na(c5)) %>%
-    purrr::set_names(variables)
+  ecodf <- tibble::tibble(c1, c2, c3, c4, c5 = as.vector(ar), c6) %>%
+    purrr::set_names(c(variables, "id")) %>%
+    dplyr::filter_(lazyeval::interp(~!is.na(var), var = as.name(variables[length(variables)])))
 
   return(ecodf)
 }
 
+# df <- vol %>%
+#   dplyr::filter(depth == 5 & time == 0) %>%
+#   ggplot2::ggplot(ggplot2::aes(x = longitude, y = latitude, label = id)) +
+#   ggplot2::geom_text(size = 2)
 
