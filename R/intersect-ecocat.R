3a463763a71784b4b7af10644e0c0116d025b65b
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-17 18:14:35 +0100

    Add intersect calculation.

---
+++ R/intersect-ecocat.R
@@ -0,0 +1,14 @@
+#' Calculate overlap between ecoham boxes and atlantis polygons.
+#'
+#' @param bgm_df Dataframe generated with atlantistools::convert_bgm().
+#' @param ecoham_layout Dataframe with columns longitude, latidude (box mid points) and the ecoham_id for
+#' each box. See \link{create-example-dfs.R} for details.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' intersect_ecocat(bgm_df, ecoham_layout)
+intersect_ecocat <- function(bgm_df, ecoham_layout) {
+
+}
