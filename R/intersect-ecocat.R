fb37d3167709d186e916068e88c6ec3f87d02f4d
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-20 17:06:41 +0100

    Finish calculations.

--- R/intersect-ecocat.R
+++ R/intersect-ecocat.R
@@ -1,21 +1,14 @@
 #' Calculate overlap between ecoham boxes and atlantis polygons.
 #'
 #' @param atlantis_bgm bgm file of the ATLANTIS model.
-#' @param ecoham_layout any netcdf file of ECOHAM grid.
+#' @param ecoham_layout dataframe of the ECOHAM layout.
 #'
 #' @return Dataframe
 #' @export
 #'
 #' @examples
 #' atlantis_bgm <- system.file(package = "ecocat", "extdata/NorthSea.bgm")
-#' ecoham_nc <- system.file(package = "ecocat", "extdata/volume.nc")
-#' atlantis_df <- rbgm::bgmfile(x = ) %>%
-#'   rbgm::boxSpatial()
-#'
-#' ecoham_df <- raster::rasterFromXYZ(xyz = ecoham_layout, digits = 3) %>%
-#'   raster::rasterToPolygons()
-#' raster::projection(ecoham_df) <- sp::CRS("+init=epsg:4326")
-#'   sp::spTransform(raster::projection(atlantis_df))
+#' overlap <- intersect_ecocat(atlantis_bgm, ecoham_layout)
 #'
 #' test <- raster::intersect(atlantis_df, ecoham_df)
 intersect_ecocat <- function(atlantis_bgm, ecoham_layout) {
@@ -29,31 +22,35 @@ intersect_ecocat <- function(atlantis_bgm, ecoham_layout) {
   # grid is not perfectly regular, use projection from bgm file.
   ecoham_spdf <- raster::rasterFromXYZ(xyz = ecoham_layout, digits = 3) %>%
     raster::rasterToPolygons()
-  # hard code projection into raster object
+  # hard code reference system and projection into raster object. Use bgm projection.
   raster::projection(ecoham_spdf) <- sp::CRS("+init=epsg:4326")
+  ecoham_spdf <- sp::spTransform(ecoham_spdf, raster::projection(atlantis_df))
 
   # Calculate spatial overlap between polygons and grid cells.
   overlap <- raster::intersect(atlantis_spdf, ecoham_spdf)
 
 }
 
-# method 1
-atlantis_df <- rbgm::bgmfile(x = system.file(package = "ecocat", "extdata/NorthSea.bgm")) %>%
-  rbgm::boxSpatial()
+# # method 1
+# atlantis_df <- rbgm::bgmfile(x = system.file(package = "ecocat", "extdata/NorthSea.bgm")) %>%
+#   rbgm::boxSpatial()
+#
+# ecoham_df <- raster::rasterFromXYZ(xyz = ecoham_layout, digits = 3) %>%
+#   raster::rasterToPolygons()
+#
+# raster::projection(ecoham_df) <- sp::CRS("+init=epsg:4326")
+#
+# ecoham_df <- sp::spTransform(ecoham_df, raster::projection(atlantis_df))
+#
+# raster::intersect(atlantis_df, ecoham_df)
+#
+# # method 2
+# atlantis_df <- rbgm::bgmfile(x = system.file(package = "ecocat", "extdata/NorthSea.bgm")) %>%
+#   rbgm::boxSpatial()
+# ecoham_df <- raster::raster(system.file(package = "ecocat", "extdata/volume.nc")) %>%
+#   raster::rasterToPolygons() %>%
+#   sp::spTransform(raster::projection(atlantis_df))
+#
+# raster::intersect(atlantis_df, ecoham_df)
 
-ecoham_df <- raster::rasterFromXYZ(xyz = ecoham_layout, digits = 3) %>%
-  raster::rasterToPolygons()
 
-raster::projection(ecoham_df) <- sp::CRS("+init=epsg:4326")
-
-ecoham_df <- sp::spTransform(ecoham_df, raster::projection(atlantis_df))
-
-intersect(atlantis_df, ecoham_df)
-
-# method 2
-atlantis_df <- rbgm::bgmfile(x = system.file(package = "ecocat", "extdata/NorthSea.bgm")) %>%
-  rbgm::boxSpatial()
-ecoham_df <-  raster(system.file(package = "ecocat", "extdata/volume.nc")) %>%
-  raster::rasterToPolygons() %>% spTransform(projection(atlantis_df))
-
-raster::intersect(atlantis_df, ecoham_df)
