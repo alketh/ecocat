8ecc42316d38a707e4d0c01fcc658b2b9f31dda7
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-22 17:24:47 +0100

    Update documentation.

--- R/ecocat.R
+++ R/ecocat.R
@@ -3,13 +3,19 @@
 #' Assign spatial output from ECOHAM simulation to ATLANTIS polygons.
 #'
 #' @param eco_out dataframe generated with \link{eco-to-tidy}.
+#' @param col column to convert from ecoham output to atlantis output.
 #' @param nicemap_df dataframe of the nicemap.
 #' @return Dataframe.
 #' @export
 #'
 #' @examples
-#' ecocat(ref_vol, nicemap_df)
+#' eco_out <- ref_vol
+#' ecocat(ref_vol, col = "vol", nicemap_df)
 
-ecocat <- function(eco_out, nicemap_df) {
-  NULL
+ecocat <- function(eco_out, col, nicemap_df) {
+  wuwu <- dplyr::left_join(eco_out, nicemap_df, by = "ecoham_id") %>%
+    dplyr::filter_(~!is.na(polygon)) %>%
+    atlantistools::agg_data(data = ., col = col, groups = c("time", "polygon"), out = col, fun = mean)
+
+  return(wuwu)
 }
