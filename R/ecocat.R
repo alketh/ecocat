06b8a75301eb22675564014c2dea4f04f4237fff
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-22 16:08:23 +0100

    Add ecocat skeleton.

---
+++ R/ecocat.R
@@ -0,0 +1,15 @@
+#' Convert ECOHAM output to ATLANTIS output.
+#'
+#' Assign spatial output from ECOHAM simulation to ATLANTIS polygons.
+#'
+#' @param eco_out dataframe generated with \link{eco-to-tidy}.
+#' @param nicemap_df dataframe of the nicemap.
+#' @return Dataframe.
+#' @export
+#'
+#' @examples
+#' ecocat(ref_vol, nicemap_df)
+
+ecocat <- function(eco_out, nicemap_df) {
+  NULL
+}
