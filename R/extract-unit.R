17436a8f999476016c09a0a1264b0d7e80f38d90
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-22 17:24:31 +0100

    Add unit extraction function.

---
+++ R/extract-unit.R
@@ -0,0 +1,27 @@
+#' Extract units for each variable in a netcdf file
+#'
+#' Extract units for each variable in a netcdf file
+#'
+#' @inheritParams eco_to_tidy
+#' @export
+
+#' @examples
+#' nc <- system.file(package = "ecocat", "extdata/volume.nc")
+#' extract_unit(nc)
+
+extract_unit <- function(nc) {
+  nc_read <- RNetCDF::open.nc(con = nc)
+  on.exit(RNetCDF::close.nc(nc_read))
+
+  nc_inq <- RNetCDF::file.inq.nc(nc_read)
+
+  # Exract units from netcdf file!
+  units <- purrr::map_chr(0:(nc_inq$nvars - 1), ~RNetCDF::att.get.nc(.x, ncfile = nc_read, attribute = "units"))
+
+  variables <- purrr::map(0:(nc_inq$nvars - 1), ~RNetCDF::var.inq.nc(.x, ncfile = nc_read)) %>%
+    purrr::map_chr(., "name")
+
+  units <- purrr::set_names(units, variables)
+
+  return(units)
+}
