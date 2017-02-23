f69c17221677391dc4de4356604a76b229672fb5
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-23 16:31:22 +0100

    Outcomment interactive code.

--- tests/testthat/test-eco-to-tidy.R
+++ tests/testthat/test-eco-to-tidy.R
@@ -1,32 +1,27 @@
 context("test specific values in dataframe")
 
-library("dplyr")
-library("ggplot2")
-library("rbgm")
-
 vol <- eco_to_tidy(system.file(package = "ecocat", "extdata/volume.nc"))
 
 test_that("test this", {
   expect_equal(dim(vol), c(17638, 6))
 })
 
-# compare ecoham/atlantis/reality layout
-lon_min <- -8
-lon_max <- 15
-lat_min <- 47
-lat_max <- 63
-
-surface_ecoham <- filter(vol, depth == 5) %>%
-  filter(longitude > lon_min)
-
-
-
-cm <- fortify(maps::map(database = "world", plot = FALSE, fill = T, xlim = c(lon_min, lon_max), ylim = c(lat_min, lat_max)))
-
-ggplot(bgm_df, aes(x = long, y = lat)) +
-  geom_polygon(data = cm, aes(group = group), colour = "black", fill = "grey") +
-  geom_polygon(aes(group = polygon), fill = NA, colour = "black") +
-  geom_point(data = surface_ecoham, aes(x = longitude, y = latitude), size = .5) +
-  coord_map(xlim = c(lon_min, lon_max), ylim = c(lat_min, lat_max))
-
-
+# compare ecoham/atlantis/reality layout.
+# Can only be used interactively.
+# lon_min <- -8
+# lon_max <- 15
+# lat_min <- 47
+# lat_max <- 63
+#
+# surface_ecoham <- dplyr::filter(vol, depth == 5) %>%
+#   dplyr::filter(longitude > lon_min)
+#
+# cm <- ggplot2::fortify(maps::map(database = "world", plot = FALSE, fill = T, xlim = c(lon_min, lon_max), ylim = c(lat_min, lat_max)))
+#
+# ggplo2::ggplot(bgm_df, ggplo2::aes(x = long, y = lat)) +
+#   ggplo2::geom_polygon(data = cm, ggplo2::aes(group = group), colour = "black", fill = "grey") +
+#   ggplo2::geom_polygon(ggplo2::aes(group = polygon), fill = NA, colour = "black") +
+#   ggplo2::geom_point(data = surface_ecoham, ggplo2::aes(x = longitude, y = latitude), size = .5) +
+#   ggplo2::coord_map(xlim = c(lon_min, lon_max), ylim = c(lat_min, lat_max))
+#
+# data(world2HiresMapEnv)
