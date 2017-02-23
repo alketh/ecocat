cef6fb13dd6c4ebb3849f000ad5df6d2e2c8ac00
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-23 15:26:29 +0100

    Update creation of example netcdf files.

--- data-raw/compress-nc.R
+++ data-raw/compress-nc.R
@@ -1,16 +1,18 @@
-# DO NOT RUN THIS!
+# DO NOT RUN THIS (unless you know what you are doing)!
 
-library("purrr")
+# Compress ECOHAM output down to 2 depth layers and 2 timesteps.
 
+nc <- "z:/my_data_alex/Markus/ECOHAM_B057/B057-D4_3D.2000.d1n.nc"
 nc <- "z:/my_data_alex/Markus/ECOHAM_B057/B057-D4_3D.2000.vol.nc"
+
 nc_read <- RNetCDF::open.nc(nc)
 
 nc_inq <- RNetCDF::file.inq.nc(nc_read)
-nvars <- RNetCDF::file.inq.nc(nc_read)$nvars
+# nvars <- RNetCDF::file.inq.nc(nc_read)$nvars
 
-nc_detail <- map(0:(nc_inq$nvars - 1), ~RNetCDF::var.inq.nc(.x, ncfile = nc_read))
+nc_detail <- purrr::map(0:(nc_inq$nvars - 1), ~RNetCDF::var.inq.nc(ncfile = nc_read, variable = .x))
 
-variables <- map_chr(nc_detail, "name")
+variables <- purrr::map_chr(nc_detail, "name")
 
 v1 <- RNetCDF::var.get.nc(nc_read, variable = variables[5], na.mode = 0)
 
@@ -26,12 +28,14 @@ for (k in 1:dim(v_test)[4]) {
   }
 }
 
-v_string <- map_chr(v_string, paste0, ",")
+v_string <- purrr::map_chr(v_string, paste0, ",")
 
-# walk(v_string, cat, file = "test.cdf", sep = "\n", append = T)
+purrr::walk(v_string, cat, file = "test_d1.cdf", sep = "\n", append = T)
 
 # very messy postprocessing needed afterwards
 # 1. Change NA to "_"
-# 2. Convert to netcdf with ncgen
-# 3. Check netcdf structure with ncdump
-# 4. Rename final file to volume.nc
+# 2. Copy to cdf skeleton.
+# 3. Update "VARNAME" with associated unit
+# 4. Convert to netcdf with ncgen
+# 5. Check netcdf structure with ncdump
+
