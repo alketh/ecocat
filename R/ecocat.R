1ab4fcb418c60b7e508c5c884d415d14e8a1f73e
alketh <Alexander.Keth@uni-hamburg.de>
2017-02-23 14:51:45 +0100

    Add conversion from mmol to mg.

--- R/ecocat.R
+++ R/ecocat.R
@@ -2,17 +2,31 @@
 #'
 #' Assign spatial output from ECOHAM simulation to ATLANTIS polygons.
 #'
-#' @param eco_out dataframe generated with \link{eco-to-tidy}.
-#' @param col column to convert from ecoham output to atlantis output.
+#' @inheritParams eco_to_tidy
 #' @param nicemap_df dataframe of the nicemap.
 #' @return Dataframe.
 #' @export
 #'
 #' @examples
-#' eco_out <- ref_vol
-#' ecocat(ref_vol, col = "vol", nicemap_df)
+#' nc <- system.file(package = "ecocat", "extdata/volume.nc")
+#' df <- ecocat(nc, nicemap_df)
 
-ecocat <- function(eco_out, col, nicemap_df) {
+ecocat <- function(nc, nicemap_df) {
+  # read in data and convert to tidy dataframe
+  eco_tidy <- eco_to_tidy(nc = nc)
+
+  # extract variable name and units
+  unit <- extract_unit(nc = nc)
+
+  # Convert from mmol to mg! By default variable is listed at the end.
+  # Atomar mass of nitrogen = 14.0067 g/mol
+  # from mmol to mol --> divide by 1000
+  # from g to mg     --> multiply by 1000
+  # Thus x [mmol] * 14.0067 [g/mol] / 1000 * 1000
+  if (unit[length(unit)] == "mmol") {
+    eco_tidy$ecoham_out <- eco_tidy$ecoham_out * 14.0067
+  }
+
   wuwu <- dplyr::left_join(eco_out, nicemap_df, by = "ecoham_id") %>%
     dplyr::filter_(~!is.na(polygon)) %>%
     atlantistools::agg_data(data = ., col = col, groups = c("time", "polygon"), out = col, fun = mean)
