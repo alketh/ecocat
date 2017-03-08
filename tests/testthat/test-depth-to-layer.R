65dd5be6ff0205f6e15d54b8e21e66b4014d428b
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-08 11:51:20 +0100

    Update tests.

--- tests/testthat/test-depth-to-layer.R
+++ tests/testthat/test-depth-to-layer.R
@@ -31,3 +31,28 @@ test_that("move layer through wc", {
 })
 
 
+head(nominal_dz_df)
+head(poly_depth_df)
+
+ndf <- tibble::tibble(polygon = 5, layer = 2:0, max_nominal_dz = c(20, 30, 40))
+pdf1 <- tibble::tibble(depth = seq(0, 60, by = 3), polygon = 5)
+
+pdf2 <- pdf1
+pdf2$depth[pdf2$depth == 6] <- 5
+pdf2 <- dplyr::filter(pdf2, depth >= 5)
+
+pdf3 <- tibble::tibble(depth = c(5, 12, seq(15, 50, by = 3)), polygon = 5)
+tdf <- depth_to_layer(pdf3, ndf)
+# Visually inspect data!
+tdf_ch <- tdf
+tdf_ch$depth_min <- c(0,  10, cumsum(rep(c(4, 2), times = nrow(pdf3) / 2 - 1)) + 10)
+tdf_ch$depth_max <- c(tdf_ch$depth_min[-1], 50)
+
+test_that("layer assignment", {
+  expect_error(depth_to_layer(pdf1, ndf), "Minimum depth is not equal to 5.")
+  expect_error(depth_to_layer(pdf2, ndf), "Pls check mean depth in ecoham grid.")
+  expect_equal(nrow(pdf3), nrow(tdf))
+  expect_equal(ncol(pdf3) + 2, ncol(tdf))
+  expect_equal(tdf$layer, c(rep(2, 4), rep(1, 4), rep(0, 3), rep(NA, 3)))
+})
+
