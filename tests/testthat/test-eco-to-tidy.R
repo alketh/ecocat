b8b53d1e92a39a2a7dcd0c73e6992ad2be42e925
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-22 17:01:57 +0100

    Add visual inspection.

--- tests/testthat/test-eco-to-tidy.R
+++ tests/testthat/test-eco-to-tidy.R
@@ -25,3 +25,37 @@ test_that("test this", {
 #   ggplo2::coord_map(xlim = c(lon_min, lon_max), ylim = c(lat_min, lat_max))
 #
 # data(world2HiresMapEnv)
+
+# # Visually inspect ECOHAM output
+# nc <- "z:/my_data_alex/Markus/ECOHAM_B057/B057-D4_3D.2000.vol.nc"
+#
+# raw <- eco_to_tidy(nc)
+#
+# # select initial time and surface plot ecohamid and compare with layout
+# wuwu <- dplyr::filter(raw, time == 0 & depth == 5)
+#
+# ggplot2::ggplot(wuwu, ggplot2::aes(x = longitude, y = latitude, label = ecoham_id)) +
+#   ggplot2::geom_text(size = 2)
+#
+# ggplot2::ggplot(ecoham_layout, ggplot2::aes(x = longitude, y = latitude, label = ecoham_id)) +
+#   ggplot2::geom_text(size = 2)
+#
+# # select different time steps and plot maximum depth.
+# wuwu <- dplyr::filter(raw, time == 0) %>%
+#   atlantistools::agg_data(col = "depth", groups = c("longitude", "latitude"), data = ., out = "depth", fun = max)
+#
+# ggplot2::ggplot(wuwu, ggplot2::aes(x = longitude, y = latitude, fill = depth)) +
+#   ggplot2::geom_tile()
+#
+# wuwu <- dplyr::filter(raw, time == 21) %>%
+#   atlantistools::agg_data(col = "depth", groups = c("longitude", "latitude"), data = ., out = "depth", fun = max)
+#
+# ggplot2::ggplot(wuwu, ggplot2::aes(x = longitude, y = latitude, fill = depth)) +
+#   ggplot2::geom_tile()
+#
+# # Plot Surface volume at time 50.
+# wuwu <- dplyr::filter(raw, time == 52 & depth == 17.5)
+#
+# ggplot2::ggplot(wuwu, ggplot2::aes(x = longitude, y = latitude, fill = ecoham_out)) +
+#   ggplot2::geom_tile()
+
