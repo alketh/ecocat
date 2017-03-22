3f115b986afd86101023eeb5071dffa24946cd9a
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-22 13:25:06 +0100

    Use filter.Tradeoff between readability and speed.

--- R/eco-to-tidy.R
+++ R/eco-to-tidy.R
@@ -36,8 +36,6 @@ eco_to_tidy <- function(nc) {
 
   # create columns (vectorised) with netcdf variable values.
   vars_new <- purrr::map(variables[-length(variables)], RNetCDF::var.get.nc, ncfile = nc_read)
-  c5 <- as.vector(ar)
-  remove_nas <- !is.na(c5)
 
   # c1 <- rep(rep(rep(vars_new[[1]], times = d[2]), times = d[3]), times = d[4])
   # c2 <- rep(rep(rep(vars_new[[2]], each = d[1]), times = d[3]), times = d[4])
@@ -45,20 +43,11 @@ eco_to_tidy <- function(nc) {
   c4 <- rep(vars_new[[4]], each = d[1] * d[2] * d[3])
   c6 <- rep(1:(d[1] * d[2]), times = d[3] * d[4])
 
-  # Filter vectors
-  # c1 <- c1[remove_nas]
-  # c2 <- c2[remove_nas]
-  # c3 <- c3[remove_nas]
-  # c4 <- c4[remove_nas]
-  # c5 <- c5[remove_nas]
-  # c6 <- c6[remove_nas]
 
   # Combine to tibble and remove missing values.
-  ecodf <- tibble::tibble(c3, c4, c5, c6)
+  ecodf <- tibble::tibble(c3, c4, as.vector(ar), c6)
   ecodf <- purrr::set_names(ecodf, c(variables[c(-1, -2, -length(variables))], "ecoham_out", "ecoham_id"))
   ecodf <- dplyr::filter_(ecodf, ~!is.na(ecoham_out))
-  # ecodf$longitude <- NULL
-  # ecodf$latitude <- NULL
 
   return(ecodf)
 }
